611a2b39822e30212f9febfb70953efe246d1606
nosmokingsurfer <nosmokingsurfer@gmail.com>
2019-05-27 12:19:35 +0300

    prepared for submit

--- main.cpp
+++ main.cpp
@@ -1,88 +1,13 @@
-#include <fstream>
 #include <string>
 #include <iostream>
 #include <vector>
 #include <set>
-
-//#include <pcl/common/common_headers.h>
-//#include <pcl/visualization/pcl_visualizer.h>
-//
-//#include <pcl/segmentation/sac_segmentation.h>
-//#include <pcl/ModelCoefficients.h>
-
-#include <random> // for std::mt19937
-#include <ctime> // for std::time
-
+#include <random>
+#include <ctime>
+#include <fstream>
+#include <chrono>
 
 using namespace std;
-//using namespace pcl;
-
-/*
-void doTest(const string& filename)
-{ 
-  cout << "Test " << filename << " started" << endl;
-  float p;
-
-  ifstream myfile;
-  myfile.open(filename+".txt");
-
-  if(!myfile.is_open())
-  {
-    cout << "file " << filename << ".txt" <<" do ot exsist" << endl;
-    return;
-  }
-
-  myfile >> p;
-
-  int N;
-  myfile >> N;
-
-  PointCloud<PointXYZ>::Ptr cloud(new PointCloud<PointXYZ>());
-  cloud->points.resize(N, { 0,0,0 });
-
-  for (auto& p : cloud->points)
-  {
-    myfile >> p.x >> p.y >> p.z;
-  }
-
-  myfile.close();
-
-  cout << "Cloud size = " << cloud->size() << endl;
-
-  ModelCoefficients model;
-  PointIndices::Ptr inliers(new PointIndices);
-  SACSegmentation<PointXYZ> seg;
-
-  seg.setInputCloud(cloud);
-  seg.setMethodType(SAC_RANSAC);
-  seg.setModelType(SACMODEL_PLANE);
-  seg.setDistanceThreshold(p);
-  seg.setOptimizeCoefficients(true);
-  seg.segment(*inliers, model);
-
-  visualization::PCLVisualizer::Ptr viewer(new visualization::PCLVisualizer("viewer"));
-
-  viewer->addPointCloud(cloud);
-  viewer->setPointCloudRenderingProperties(visualization::PCL_VISUALIZER_POINT_SIZE, 3);
-  viewer->addPlane(model);
-  while (!viewer->wasStopped())
-  {
-    viewer->spinOnce();
-  }
-
-  ofstream output;
-  output.open(filename + "_out.txt");
-  output.clear();
-  for(const auto& coeff : model.values)
-  {
-    output << coeff << " ";
-  }
-  output.close();
-
-}
-*/
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 typedef vector<vector<float>> Matrix;
 
@@ -94,7 +19,7 @@ struct Point
 
   Point& normalize()
   {
-    float sum = pow(x*x + y*y + z*z, 0.5);
+    float sum = powf(x*x + y*y + z*z, 0.5);
 
     this->x /= sum;
     this->y /= sum;
@@ -111,9 +36,24 @@ struct PlaneModel
   float C;
   float D;
 
+  PlaneModel(){}
+
+  inline bool operator== (const PlaneModel& rhs)
+  {
+    float eps = 0.00001;
+    return ((fabs(A - rhs.A) < eps) && (fabs(B - rhs.B) < eps) && (fabs(C - rhs.C) < eps) && (fabs(D - rhs.D) < eps));
+  }
+
+  PlaneModel(const float a, const float b, const float c, const float d)
+  {
+    A = a;
+    B = b;
+    C = c;
+    D = d;
+  }
+
   void print()
   {
-    cout << "Plane model:" << endl;
     cout << A << " " << B << " " << C << " " << D << endl;
   }
 
@@ -121,12 +61,19 @@ struct PlaneModel
   {
     float result = {0};
 
-    result = (fabs(A*p.x +B*p.y + C*p.z + D))/(pow(A*A + B*B + C*C, 0.5));
+    result = (fabs(A*p.x +B*p.y + C*p.z + D))/(powf(A*A + B*B + C*C, 0.5));
 
     return result;
   }
-};
 
+  bool isValid()
+  {
+    if((A == 0)&&(B == 0)&&(C == 0)&&(D == 0))
+      return false;
+    else
+      return true;
+  }
+};
 
 Point operator*(const Matrix& lhs, const Point& rhs)
 {
@@ -152,7 +99,6 @@ void printMatrix(const Matrix& m)
   }
 }
 
-// @brief return Center Of Mass for points points
 Point getCOM(const vector<Point>& points)
 {
     Point result = {0,0,0};
@@ -164,7 +110,7 @@ Point getCOM(const vector<Point>& points)
       result.z += p.z;
     }
 
-    int N = points.size();
+    auto N = points.size();
 
     result.x /= N;
     result.y /= N;
@@ -186,7 +132,7 @@ Point getCOM(const vector<Point>& points, const set<int> indices)
       result.z += points[i].z;
     }
 
-    int N = indices.size();
+    auto N = indices.size();
 
     result.x /= N;
     result.y /= N;
@@ -199,7 +145,6 @@ Point getCOM(const vector<Point>& points, const set<int> indices)
 
   return result;
 }
-
 
 void getMoments(const vector<Point>& points, Point& com, Matrix& inertia)
 {
@@ -267,10 +212,6 @@ vector<vector<float>> getAdjointMatrix(const vector<vector<float>>& m)
 {
   vector<vector<float>> result = {{0,0,0},{0,0,0},{0,0,0}};
 
-  // a b c
-  // d e f
-  // g h i
-
   float a = m[0][0];
   float b = m[0][1];
   float c = m[0][2];
@@ -312,12 +253,11 @@ float checkModelQuality(const float thresh, const vector<Point>& cloud, const Pl
 
 set<int> getMayBeInliers(const vector<Point>& cloud, const int num)
 {
-  int size = cloud.size();
+  auto size = cloud.size();
 
-  // Initialize our mersenne twister with a random seed based on the clock
-  std::mt19937 mersenne(static_cast<std::mt19937::result_type>(std::time(nullptr)));
+  std::mt19937 mersenne(static_cast<std::mt19937::result_type>(chrono::high_resolution_clock::now().time_since_epoch().count()));
 
-  std::uniform_int_distribution<> die(0, size - 1);
+  std::uniform_int_distribution<> die(0, static_cast<int>(size) - 1);
 
   set<int> result;
 
@@ -341,13 +281,12 @@ PlaneModel fitModel(const vector<Point>& data, const set<int> indices = {})
   getMoments(data, indices, com, inertia);
   Matrix adjoined = getAdjointMatrix(inertia);
 
-  Point eigenVector = { 1,2,3 };//some random vector
+  Point eigenVector = { 1,2,3 };
 
   for (auto i = 0; i < 5; i++)
   {
     eigenVector = adjoined*eigenVector;
     eigenVector.normalize();
-    
   }
 
   model.A = eigenVector.x;
@@ -358,24 +297,27 @@ PlaneModel fitModel(const vector<Point>& data, const set<int> indices = {})
   return model;
 }
 
-PlaneModel RANSAC(const vector<Point>& data, const float thresh, const int d, const float n = 0.8, const int k = 10)
+PlaneModel RANSAC(const vector<Point>& data, const float thresh, const float n = 0.55, const int k = 10)
 {
-  PlaneModel bestFit = { 0,0,0,0 };
+  PlaneModel bestFit = {0,0,0,0};
 
   bool bestFirInitialized = false;
 
-  int size = data.size();
+  auto size = data.size();
 
   int iterations = 0;
-  float bestError = 1.0*size;
+  float bestError = 1.0f*size;
+  float prevBestError = 2* bestError;
 
   while (iterations < k)
   {
     set<int> mayBeInliers = getMayBeInliers(data, (int)(size*n));
+
     PlaneModel mayBeModel = fitModel(data, mayBeInliers);
 
     if(!bestFirInitialized)
       bestFit = mayBeModel;
+      bestFirInitialized = true;
 
     set<int> alsoInliers = {};
 
@@ -388,13 +330,11 @@ PlaneModel RANSAC(const vector<Point>& data, const float thresh, const int d, co
           alsoInliers.insert(i);
         }
       }
-      if(mayBeInliers.size() + alsoInliers.size() == size)
-      {
-        break;
-      }
     }
 
-    if(alsoInliers.size() > d)
+
+
+    if(alsoInliers.size() + mayBeInliers.size() >= size/2)
     {
       alsoInliers.insert(begin(mayBeInliers), end(mayBeInliers));
       PlaneModel betterModel = fitModel(data, alsoInliers);
@@ -404,9 +344,12 @@ PlaneModel RANSAC(const vector<Point>& data, const float thresh, const int d, co
       if(thisErr < bestError)
       {
         bestFit = betterModel;
+        prevBestError = bestError;
         bestError = thisErr;
       }
     }
+    else if (fabs(prevBestError - bestError) < 0.0001)
+      break;
 
     iterations++;
   }
@@ -414,62 +357,73 @@ PlaneModel RANSAC(const vector<Point>& data, const float thresh, const int d, co
   return bestFit;
 }
 
-void solve(const string& filename)
+void readData(float& p, int& N, vector<Point>& data)
 {
-  cout << "Test " << filename << " started" << endl;
-  float p;
+  cin >> p;
+  cin >> N;
 
-  ifstream myfile;
-  myfile.open(filename + ".txt");
+  data.resize(N,{0,0,0});
 
-  if (!myfile.is_open())
+  for(auto& p: data)
   {
-    cout << "file " << filename << ".txt" << " do ot exsist" << endl;
-    return;
+    cin >> p.x;
+    cin >> p.y;
+    cin >> p.z;
   }
-
-  myfile >> p;
-
-  int N;
-  myfile >> N;
-
-  vector<Point> cloud;
-  cloud.resize(N, { 0,0,0 });
-
-  for (auto& p : cloud)
-  {
-    myfile >> p.x >> p.y >> p.z;
-  }
-  myfile.close();
-  ///////////////////////////////////////////////////////////////
-
-
-  PlaneModel solution;
-  if(cloud.size() > 100)
-  { 
-    solution = RANSAC(cloud, p, (int)(cloud.size()*0.05), 0.6f, 20);
-  }
-  else
-  {
-    solution = fitModel(cloud);
-  }
-
-  solution.print();
-
 }
 
+void readData(float& p, int& N, vector<Point>& data, const string& filename)
+{
+  ifstream myFile;
+  myFile.open(filename);
+  
+  myFile >> p;
+  myFile >> N;
+
+  data.resize(N, { 0,0,0 });
+
+  for (auto& p : data)
+  {
+    myFile >> p.x;
+    myFile >> p.y;
+    myFile >> p.z;
+  }
+
+  myFile.close();
+}
+
+void solve(const float p, const int N, const vector<Point> data)
+{
+  PlaneModel solution;
+
+  if(data.size() >= 6)
+  {
+    solution = RANSAC(data, p, 0.5f, 30);  
+  }
+  else if(data.size() >=3)
+  {
+    solution = fitModel(data);
+  }
 
 
-
+  solution.print();
+}
 
 int main() {
+  float p = 0;
+  int N = 0;
+  vector<Point> data;
 
-  for(const auto& test : {"test_0", "test_1", "test_2", "test_3", "sdc_point_cloud"})
-  {
-    //doTest(test);
-    solve(test);
-    
-  }
+  
+  
+  //for(auto test: {"test_0.txt", "test_1.txt", "test_2.txt", "test_3.txt", "sdc_point_cloud.txt"})
+  //{ 
+  //  readData(p, N, data, test);
+  //  solve(p,N,data); 
+  //}
+
+  readData(p,N,data);
+  solve(p,N,data);
 
 
   return 0;
